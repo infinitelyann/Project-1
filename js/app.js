1156aeba21ac25e6452c8334d6addc798b372c31
saparker <stephanieparker1997@gmail.com>
2022-09-21 11:46:07 -0400

    dealer and player cards display

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 
 const deckUrl = "https://deckofcardsapi.com/api/deck/new/draw/?count=52"
-let player = document.getElementById('player-button')
+let playerBet = document.getElementById('player-button')
 const dealerTable = document.getElementById('dealer')
-let dealer = false
-let playerDraw = []
-let dealerDraw = [0, 0]
+let playerCard1 = document.getElementById("player-img1")
+let playerCard2 = document.getElementById("player-img2")
+let dealerCard1 = document.getElementById("dealer-img1")
+let dealerCard2 = document.getElementById("dealer-img2")
+let deck = []
+let player = []
+let dealer = []
+
+let cardImgUrls = []
 
 
-
-let deck = [];
-let cards = [];
 async function getJson(url) {
     let response = await fetch(url);
     let data = await response.json()
@@ -17,30 +20,44 @@ async function getJson(url) {
 }
 
 async function main(){
-    deck = await getJson(deckUrl)
-    cards = deck.cards.map(card =>{
-            return {
-                value: card.value,
-                image: card.image,
-            }
-    })  
+    data = await getJson(deckUrl)
+     deck = data.cards
+    cardImgUrls = deck.map(cards=>{
+        return{
+            image:cards.image
+        }
+    })
+   
 }
+
 main()
 
-player.addEventListener('click', ()=>{
-    playerDraw[0] = cards[0]
-    playerDraw[1] = cards[1]
-    cards.shift();
-    cards.shift();
-    console.log(playerDraw)
-    console.log(cards)
-    document.getElementById('player-button').disabled = true;
-})
+ function dealingCards(user){ 
 
+   user.push(deck.pop())
+   cardImgUrls.pop()
+   user.push(deck.pop())
+   cardImgUrls.pop()
+   console.log(user)
+   
+ }
 
+    
+    playerBet.addEventListener('click',() =>{
+            dealingCards(player)
+            dealingCards(dealer)
+            playerCard1.src = player[0].image
+            playerCard2.src = player[1].image
+            dealerCard1.src = dealer[0].image
+            dealerCard2.src = dealer[1].image
+        
+           })
 
-
-
+//  function gettingImages(user){
+//             for(let i =0; i <=user.length; i++){
+              
+//             }
+//         }       
 
 
 
